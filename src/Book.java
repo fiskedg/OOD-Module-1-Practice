89698291603fc8210f6b9721794b958437291d0e
rau <jackrau06@gmail.com>
2026-01-15 11:45:19 -0500

    Book class

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private int edition;
+    private int copiesAvailable;
+    
+    public Book(int bookId, String title, String author, int edition, int copiesAvailable){
+        this.bookId=bookId;
+        this.title=title;
+        this.author=author;
+        this.edition=edition;
+        this.copiesAvailable=copiesAvailable;
+    }
+
+    public void showDetails(){
+        System.out.println(bookId);
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(edition);
+        System.out.println(copiesAvailable);
+    }
+}
